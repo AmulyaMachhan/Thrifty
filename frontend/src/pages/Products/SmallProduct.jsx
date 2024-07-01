91251f0baf082d7db52dbdc7c9709f6e418ab36f
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-01 12:37:13 +0530

    Updated the UI

--- frontend/src/pages/Products/SmallProduct.jsx
+++ frontend/src/pages/Products/SmallProduct.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import HeartIcon from "./HeartIcon";
+import HeartIcon from "../Favourites/HeartIcon";
 
 const SmallProduct = ({ product }) => {
   return (
