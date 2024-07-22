4afaa94c5313e04f48ed54532d1d93c5524addba
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-22 11:50:13 +0530

    Delete Backend

--- backend/utils/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-const asyncHandler = (requestHandler) => (req, res, next) => {
-  Promise.resolve(requestHandler(req, res, next)).catch((error) =>
-    res.status(500).json({
-      message: error.message,
-    })
-  );
-};
-
-export { asyncHandler };
