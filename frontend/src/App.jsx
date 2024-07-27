3aa364843106201902aae97b79d4a067f5fd06cf
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-27 12:45:34 +0530

    Added router provide and main route in main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,15 @@ import { ToastContainer } from "react-toastify";
 import { Outlet } from "react-router-dom";
 import Navigation from "./pages/Auth/Navigation.jsx";
 import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <>
       <ToastContainer />
+      <Navigation />
+      <main className="py-3">
+        <Outlet />
+      </main>
     </>
   );
 }
---
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -0,0 +1,9 @@
+function Navigation() {
+  return (
+    <div>
+      <h1>Navigation</h1>
+    </div>
+  );
+}
+
+export default Navigation;
