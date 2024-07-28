5955e5e9f088161df00601c36d062778c5a62214
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-28 13:15:28 +0530

    Added auth reducer to the store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
-import { apiSlice } from "./api/apiSlice";
+import { apiSlice } from "./api/apiSlice.js";
+import authReducer from "./features/authSlice.js";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
   },
 
   middleware: (getDefaultMiddleware) =>
