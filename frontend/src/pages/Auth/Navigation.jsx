6ae2bd725f8a043a873d3f061a8ad4037d02ed09
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-28 15:23:41 +0530

    Fixed bugs in Navigation.jsx

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -9,29 +9,37 @@ import {
   AiOutlineShoppingCart,
 } from "react-icons/ai";
 import { FaHeart } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useLoginMutation } from "../../redux/api/userApiSlice.js";
+import { logout } from "../../redux/features/authSlice.js";
 
 function Navigation() {
+  const { userInfo } = useSelector((state) => state.auth);
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [showSidebar, setShowSidebar] = useState(false);
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
   };
 
-  const toggleSidebar = () => {
-    setShowSidebar(!showSidebar);
-  };
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const closeSidebar = () => {
-    setShowSidebar(false);
-  };
+  const [logoutApiCall] = useLoginMutation();
 
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div
       style={{ zIndex: 999 }}
-      className={`${showSidebar ? "hidden" : "flex"} 
-        xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-black 
-        w-[4%] hover:w-[15%] h-[100vh] fixed`}
+      className={`xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-black w-[4%] hover:w-[15%] h-[100vh] fixed`}
       id="navigation-container"
     >
       <div className="flex flex-col justify-center space-y-4">
@@ -66,6 +74,19 @@ function Navigation() {
           <FaHeart className="mr-2 mt-[3rem]" size={26} />
           <span className="hidden nav-item-name mt-[3rem]">FAVOURITES</span>
         </Link>
+      </div>
+
+      <div className="realtive">
+        <button
+          onClick={toggleDropdown}
+          className="flex items-center text-gray-800 focus:outline-none"
+        >
+          {userInfo ? (
+            <span className="text-white">{userInfo.username}</span>
+          ) : (
+            <></>
+          )}
+        </button>
       </div>
 
       <ul>
