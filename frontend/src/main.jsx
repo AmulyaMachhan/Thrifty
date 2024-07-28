78fafcc137e0ab0a7e7c6333423667d7c0c53b24
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-28 13:45:49 +0530

    Added store provider for redux in the main.jsx file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,16 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(<Route path="/" element={<App />}></Route>)
 );
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
