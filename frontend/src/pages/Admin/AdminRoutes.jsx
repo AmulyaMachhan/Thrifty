f85d73654deabd76e7c10ffcf4362cf3adb3e7d6
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-30 18:24:49 +0530

    Created rootelement for admin routes

---
+++ frontend/src/pages/Admin/AdminRoutes.jsx
@@ -0,0 +1,14 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+function AdminRoutes() {
+  const { userInfo } = useSelector((state) => state.auth);
+
+  return (userInfo ?? userInfo.isAdmin) ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" replace />
+  );
+}
+
+export default AdminRoutes;
