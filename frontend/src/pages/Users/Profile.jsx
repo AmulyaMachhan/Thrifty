1bd371e58be6af30b8e100153ef540cdc8d99bec
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-30 18:21:49 +0530

    Updated the profile section and fixed bugs

--- frontend/src/pages/Users/Profile.jsx
+++ frontend/src/pages/Users/Profile.jsx
@@ -4,6 +4,7 @@ import { useProfileMutation } from "../../redux/api/userApiSlice";
 import { toast } from "react-toastify";
 import { setCredentials } from "../../redux/features/authSlice";
 import Loader from "../../components/Loader";
+import { Link } from "react-router-dom";
 
 function Profile() {
   const [username, setUsername] = useState("");
@@ -38,74 +39,80 @@ function Profile() {
         }).unwrap();
 
         dispatch(setCredentials({ ...res }));
-        toast.success(res.message);
+        toast.success("Profile Updated Successfully");
       } catch (error) {
         toast.error(error?.data?.message || error.message);
       }
     }
   };
   return (
-    <div className="container mx-auto mt-[10rem]">
+    <div className="container mx-auto p-4 mt-[10rem]">
       <div className="flex justify-center align-center md:flex md:space-x-4">
-        <h2 className="text-2xl font-semibold mb-4">Update Profile</h2>
+        <div className="md:w-1/3">
+          <h2 className="text-2xl font-semibold mb-4">Update Profile</h2>
+          <form onSubmit={submitHandler}>
+            <div className="mb-4">
+              <label className="block text-white mb-2">Name</label>
+              <input
+                type="text"
+                placeholder="Enter name"
+                className="form-input p-4 rounded-sm w-full"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
 
-        <form>
-          <div className="mb-4">
-            <label className="block text-white mb-2">Name</label>
-            <input
-              type="text"
-              placeholder="Enter name"
-              className="form-input p-4 rounded-sm w-full"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
+            <div className="mb-4">
+              <label className="block text-white mb-2">Email Address</label>
+              <input
+                type="email"
+                placeholder="Enter email"
+                className="form-input p-4 rounded-sm w-full"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block text-white mb-2">Email Address</label>
-            <input
-              type="email"
-              placeholder="Enter email"
-              className="form-input p-4 rounded-sm w-full"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
+            <div className="mb-4">
+              <label className="block text-white mb-2">Password</label>
+              <input
+                type="password"
+                placeholder="Enter password"
+                className="form-input p-4 rounded-sm w-full"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block text-white mb-2">Password</label>
-            <input
-              type="password"
-              placeholder="Enter password"
-              className="form-input p-4 rounded-sm w-full"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
+            <div className="mb-4">
+              <label className="block text-white mb-2">Confirm Password</label>
+              <input
+                type="password"
+                placeholder="Confirm password"
+                className="form-input p-4 rounded-sm w-full"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block text-white mb-2">Confirm Password</label>
-            <input
-              type="password"
-              placeholder="Confirm password"
-              className="form-input p-4 rounded-sm w-full"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-          </div>
+            <div className="flex justify-between">
+              <button
+                type="submit"
+                className="bg-pink-500 text-white py-2 px-4 rounded hover:bg-pink-600"
+              >
+                Update
+              </button>
 
-          <div className="flex justify-between">
-            <button
-              type="submit"
-              onClick={submitHandler}
-              className="bg-pink-500 text-white py-2 px-4 rounded hover:bg-pink-600"
-            >
-              Update
-            </button>
-          </div>
-
-          {loadingUpdatedProfile && <Loader />}
-        </form>
+              <Link
+                to="/user-orders"
+                className="bg-pink-600 text-white py-2 px-4 rounded hover:bg-pink-700"
+              >
+                My Orders
+              </Link>
+            </div>
+            {loadingUpdatedProfile && <Loader />}
+          </form>
+        </div>
       </div>
     </div>
   );
