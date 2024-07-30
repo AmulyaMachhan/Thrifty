d62bcb4baff9e514f67ff9aaec9187868cc73bdd
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-30 18:02:06 +0530

    Created Profile Component

---
+++ frontend/src/pages/Users/Profile.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useProfileMutation } from "../../redux/api/userApiSlice";
+
+function Profile() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const [updateProfile, { isLoading: loadingUpdatedProfile }] =
+    useProfileMutation();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setEmail(userInfo.email);
+    setUsername(userInfo.username);
+  }, [userInfo.username, userInfo.email]);
+
+  return (
+    <div className="container mx-auto mt-[10rem]">
+      <div className="flex justify-center align-center md:flex md:space-x-4">
+        <h2 className="text-2xl font-semibold mb-4">Update Profile</h2>
+
+        <form>
+          <div className="mb-4">
+            <label className="block text-white mb-2">Name</label>
+            <input
+              type="text"
+              placeholder="Enter name"
+              className="form-input p-4 rounded-sm w-full"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-white mb-2">Email Address</label>
+            <input
+              type="email"
+              placeholder="Enter email"
+              className="form-input p-4 rounded-sm w-full"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-white mb-2">Password</label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              className="form-input p-4 rounded-sm w-full"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-white mb-2">Confirm Password</label>
+            <input
+              type="password"
+              placeholder="Confirm password"
+              className="form-input p-4 rounded-sm w-full"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="flex justify-between">
+            <button
+              type="submit"
+              onClick={submitHandler}
+              className="bg-pink-500 text-white py-2 px-4 rounded hover:bg-pink-600"
+            >
+              Update
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
