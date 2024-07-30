ae9d82ab1f62b18607dec4e7a58c5f6ee6cabab0
AmulyaMachhan <machhanamulya@gmail.com>
2024-07-30 17:43:15 +0530

    Added private routes to main.jsx and created Private Route Component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,29 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+
 import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
+
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+
 import Login from "./pages/Auth/Login.jsx";
 import Register from "./pages/Auth/Register.jsx";
+
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import Profile from "./pages/Users/Profile.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+
+      {/* Registered User Routes */}
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
     </Route>
   )
 );
