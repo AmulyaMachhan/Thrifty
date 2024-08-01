26c04b9bb84801bffd976e4eeb25b382c0ac93d1
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-01 16:22:57 +0530

    Fixed access for this route

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { createCategory } from "../controllers/category.controller.js";
+import {
+  authenticate,
+  authorizeAdmin,
+} from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.route("/").post(createCategory);
+router.route("/").post(authenticate, authorizeAdmin, createCategory);
 
 export default router;
