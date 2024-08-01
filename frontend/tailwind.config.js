42156d2dbfd4b642da6db5234f039024b48f9c98
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-01 12:23:12 +0530

    Added flowbite components to the UI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import flowbitePlugin from "flowbite/plugin";
+import flowbite from "flowbite-react/tailwind";
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbitePlugin, flowbite.plugin()],
 };
