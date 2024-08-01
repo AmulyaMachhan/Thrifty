ec44de5638f2592b2ba794a0d7e3e109becac062
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-01 15:58:59 +0530

    Added Category model to the backend

--- backend/models/category.model.js
+++ backend/models/category.model.js
@@ -1,8 +1,18 @@
 import mongoose, { Schema } from "mongoose";
 
 const categorySchema = new Schema(
-  {},
+  {
+    category: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      maxLength: 32,
+    },
+  },
   {
     timestamps: true,
   }
 );
+
+export const Category = mongoose.model("Category", categorySchema);
