1eab9d9a705884e725dbb1c99c245f1fea5f9812
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-03 13:35:47 +0530

    Displayed categories

--- frontend/src/pages/Admin/CategoryList.jsx
+++ frontend/src/pages/Admin/CategoryList.jsx
@@ -1,13 +1,37 @@
-import { useFetchCategoriesQuery } from "../../redux/api/categoryApiSlice";
+import { useEffect, useState } from "react";
+import CategoryForm from "../../components/CategoryForm";
+import {
+  useCreateCategoryMutation,
+  useDeleteCategoryMutation,
+  useFetchCategoriesQuery,
+  useUpdateCategoryMutation,
+} from "../../redux/api/categoryApiSlice";
+import { toast } from "react-toastify";
+import Modal from "../../components/Modal";
 
 function CategoryList() {
-  const { data: categories } = useFetchCategoriesQuery();
+  const { data: categories, refetch } = useFetchCategoriesQuery();
+
+  const [name, setName] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [updatingName, setUpdatingName] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [createCategory] = useCreateCategoryMutation();
+  const [updateCategory] = useUpdateCategoryMutation();
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
 
   return (
     <div className="flex justify-center">
+      <div></div>
       {categories?.map((category) => (
         <div key={category._id} className="text-white">
-          {category.name}
+          <button onClick={() => setModalVisible(true)}>
+            {" "}
+            {category.name}
+          </button>
         </div>
       ))}
     </div>
