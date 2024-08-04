e525d97a24d640a4f034e23a427cabac3b486e29
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-04 13:07:13 +0530

    Created a controller for updating product details

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -29,4 +29,35 @@ const addProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { addProduct };
+const updateProductDetails = asyncHandler(async (req, res) => {
+  const { name, brand, description, quantity, price, category } = req.fields;
+
+  switch (true) {
+    case !name:
+      return res.status(401).res.json({ message: "Name is required" });
+    case !brand:
+      return res.status(401).res.json({ message: "Brand is required" });
+    case !description:
+      return res.status(401).res.json({ message: "Desription is required" });
+    case !quantity:
+      return res.status(401).res.json({ message: "Quantity is required" });
+    case !price:
+      return res.status(401).res.json({ message: "Price is required" });
+    case !category:
+      return res.status(401).res.json({ message: "Category is required" });
+  }
+
+  try {
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      { ...req.fields },
+      { new: true }
+    );
+    await product.save();
+
+    return res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: "Error while updating the product" });
+  }
+});
+export { addProduct, updateProductDetails };
