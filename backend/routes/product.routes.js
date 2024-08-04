9ebb59b513f8a13feab0a45ecd4260da367fb108
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-04 13:22:24 +0530

    Created a route for updating product details

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,12 +1,19 @@
 import express from "express";
+import formidable from "express-formidable";
 import {
   authenticate,
   authorizeAdmin,
 } from "../middlewares/auth.middleware.js";
-import { addProduct } from "../controllers/product.controller.js";
+import {
+  addProduct,
+  updateProductDetails,
+} from "../controllers/product.controller.js";
 
 const router = express.Router();
 
-router.route("/").post(authenticate, authorizeAdmin, addProduct);
+router.route("/").post(authenticate, authorizeAdmin, formidable(), addProduct);
+router
+  .route("/:id")
+  .put(authenticate, authorizeAdmin, formidable(), updateProductDetails);
 
 export default router;
