6c69c416e69eaa6a7c64cf85523c9affce1bfca2
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-09 14:05:34 +0530

    Created basic layout for ProductList.jsx

--- frontend/src/pages/Admin/ProductList.jsx
+++ frontend/src/pages/Admin/ProductList.jsx
@@ -1,5 +1,20 @@
+import { useEffect } from "react";
+import { useAllProductsQuery } from "../../redux/api/productApiSlice";
+
 function ProductList() {
-  return <div></div>;
+  const { data: products, refetch } = useAllProductsQuery();
+
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
+
+  return (
+    <div className="w-full flex justify-center align-center h-full">
+      {products?.map((product) => (
+        <div key={product.id}>{product.name}</div>
+      ))}
+    </div>
+  );
 }
 
 export default ProductList;
