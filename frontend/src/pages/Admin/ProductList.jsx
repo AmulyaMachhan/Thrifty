4e222869a3896b6d81ec490407bee24be50e7bcf
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-09 14:22:14 +0530

    Created upload handler for product creation

--- frontend/src/pages/Admin/ProductList.jsx
+++ frontend/src/pages/Admin/ProductList.jsx
@@ -1,7 +1,57 @@
 import { useEffect } from "react";
-import { useAllProductsQuery } from "../../redux/api/productApiSlice";
+import {
+  useAllProductsQuery,
+  useCreateProductsMutation,
+} from "../../redux/api/productApiSlice";
+import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import { useFetchCategoriesQuery } from "../../redux/api/categoryApiSlice";
 
 function ProductList() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [brand, setBrand] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [quantity, setQuantity] = useState(0);
+  const [price, setPrice] = useState(0);
+  const [stock, setStock] = useState(0);
+  const [imageURL, setImageURL] = useState(null);
+
+  const navigate = useNavigate();
+
+  const [createProduct] = useCreateProductsMutation();
+
+  const { data: categories } = useFetchCategoriesQuery();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const productData = new FormData();
+
+    productData.append("name", name);
+    productData.append("image", image);
+    productData.append("brand", brand);
+    productData.append("description", description);
+    productData.append("category", category);
+    productData.append("quantity", quantity);
+    productData.append("price", price);
+    productData.append("countInStock", stock);
+
+    try {
+      const { data } = await createProduct(productData);
+
+      if (!data) {
+        toast.error("Product Creation Failed!!");
+        return;
+      } else {
+        toast.success(`${data.name} Created Successfully`);
+        navigate("/");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Product Creation Failed!!");
+    }
+  };
   const { data: products, refetch } = useAllProductsQuery();
 
   useEffect(() => {
@@ -9,10 +59,113 @@ function ProductList() {
   }, [refetch]);
 
   return (
-    <div className="w-full flex justify-center align-center h-full">
-      {products?.map((product) => (
-        <div key={product.id}>{product.name}</div>
-      ))}
+    <div className="container xl:mx-[9rem] sm:mx-[0]">
+      <div className="flex flex-col md:flex-row">
+        <div className="md:w-3/4 p-3">
+          <div className="h-12">Create Product</div>
+
+          <div className="mb-3">
+            <label className="border text-white px-4 block w-full text-center rounded-lg cursor-pointer font-bold py-11">
+              {image ? image.name : "Upload Image"}
+
+              <input
+                type="file"
+                name="image"
+                accept="image/*"
+                onChange={uploadFileHandler}
+                className={!image ? "hidden" : "text-white"}
+              />
+            </label>
+          </div>
+
+          <div className="p-3">
+            <div className="flex flex-wrap">
+              <div className="one">
+                <label htmlFor="name">Name</label> <br />
+                <input
+                  type="text"
+                  className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="two ml-10 ">
+                <label htmlFor="name block">Price</label> <br />
+                <input
+                  type="number"
+                  className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="flex flex-wrap">
+              <div className="one">
+                <label htmlFor="name block">Quantity</label> <br />
+                <input
+                  type="number"
+                  className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white"
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
+              </div>
+              <div className="two ml-10 ">
+                <label htmlFor="name block">Brand</label> <br />
+                <input
+                  type="text"
+                  className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white"
+                  value={brand}
+                  onChange={(e) => setBrand(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <label htmlFor="" className="my-5">
+              Description
+            </label>
+            <textarea
+              type="text"
+              className="p-2 mb-3 bg-[#101011] border rounded-lg w-[95%] text-white"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+
+            <div className="flex justify-between">
+              <div>
+                <label htmlFor="name block">Count In Stock</label> <br />
+                <input
+                  type="text"
+                  className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white"
+                  value={stock}
+                  onChange={(e) => setStock(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="">Category</label> <br />
+                <select
+                  placeholder="Choose Category"
+                  className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white"
+                  onChange={(e) => setCategory(e.target.value)}
+                >
+                  {categories?.map((c) => (
+                    <option key={c._id} value={c._id}>
+                      {c.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            <button
+              onClick={handleSubmit}
+              className="py-4 px-10 mt-5 rounded-lg text-lg font-bold bg-pink-600"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
