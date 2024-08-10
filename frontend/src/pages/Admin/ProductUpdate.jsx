68680835681234b0a94d7bdd7f4eca8200ea7cc3
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-10 13:56:45 +0530

    Created the design for product update

---
+++ frontend/src/pages/Admin/ProductUpdate.jsx
@@ -0,0 +1,139 @@
+import AdminMenu from "./AdminMenu";
+
+function ProductUpdate() {
+  return (
+    <>
+      <div className="container  xl:mx-[9rem] sm:mx-[0]">
+        <div className="flex flex-col md:flex-row">
+          <AdminMenu />
+          <div className="md:w-3/4 p-3">
+            <div className="h-12">Update / Delete Product</div>
+
+            {image && (
+              <div className="text-center">
+                <img
+                  src={image}
+                  alt="product"
+                  className="block mx-auto w-full h-[40%]"
+                />
+              </div>
+            )}
+
+            <div className="mb-3">
+              <label className="text-white  py-2 px-4 block w-full text-center rounded-lg cursor-pointer font-bold py-11">
+                {image ? image.name : "Upload image"}
+                <input
+                  type="file"
+                  name="image"
+                  accept="image/*"
+                  onChange={uploadFileHandler}
+                  className="text-white"
+                />
+              </label>
+            </div>
+
+            <div className="p-3">
+              <div className="flex flex-wrap">
+                <div className="one">
+                  <label htmlFor="name">Name</label> <br />
+                  <input
+                    type="text"
+                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white mr-[5rem]"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+
+                <div className="two">
+                  <label htmlFor="name block">Price</label> <br />
+                  <input
+                    type="number"
+                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white "
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-wrap">
+                <div>
+                  <label htmlFor="name block">Quantity</label> <br />
+                  <input
+                    type="number"
+                    min="1"
+                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white mr-[5rem]"
+                    value={quantity}
+                    onChange={(e) => setQuantity(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="name block">Brand</label> <br />
+                  <input
+                    type="text"
+                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white "
+                    value={brand}
+                    onChange={(e) => setBrand(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <label htmlFor="" className="my-5">
+                Description
+              </label>
+              <textarea
+                type="text"
+                className="p-2 mb-3 bg-[#101011]  border rounded-lg w-[95%] text-white"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+
+              <div className="flex justify-between">
+                <div>
+                  <label htmlFor="name block">Count In Stock</label> <br />
+                  <input
+                    type="text"
+                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white "
+                    value={stock}
+                    onChange={(e) => setStock(e.target.value)}
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="">Category</label> <br />
+                  <select
+                    placeholder="Choose Category"
+                    className="p-4 mb-3 w-[30rem] border rounded-lg bg-[#101011] text-white mr-[5rem]"
+                    onChange={(e) => setCategory(e.target.value)}
+                  >
+                    {categories?.map((c) => (
+                      <option key={c._id} value={c._id}>
+                        {c.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+
+              <div className="">
+                <button
+                  onClick={handleSubmit}
+                  className="py-4 px-10 mt-5 rounded-lg text-lg font-bold  bg-green-600 mr-6"
+                >
+                  Update
+                </button>
+                <button
+                  onClick={handleDelete}
+                  className="py-4 px-10 mt-5 rounded-lg text-lg font-bold  bg-pink-600"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProductUpdate;
