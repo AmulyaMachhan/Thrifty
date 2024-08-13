46c20934ad59b9be6380b7be32c0078bbd842458
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-13 14:46:43 +0530

    Created a utility function for adding  favourites to local storage

--- frontend/src/utils/localStorage.js
+++ frontend/src/utils/localStorage.js
@@ -1,3 +1,12 @@
+//Add a product to local storage
+export const addFavouriteToLocalStorage = (product) => {
+  const favourites = getFavouritesFromLocalStorage() || [];
+  if (favourites.some((p) => p._id === product._id)) {
+    favourites.push(product);
+    localStorage.setItem("favourites", JSON.stringify(favourites));
+  }
+};
+
 //Retrieve Favourites from Local Storage
 export const getFavouritesFromLocalStorage = () => {
   const favouritesJSON = localStorage.getItem("favourites");
