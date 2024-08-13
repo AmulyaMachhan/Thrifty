346c8123ae334062e7ff83c78da41900dbff35ca
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-13 14:59:53 +0530

    Updated the store embedding the preloaded state as well as favourites reducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
 import { apiSlice } from "./api/apiSlice";
+import { getFavouritesFromLocalStorage } from "../utils/localStorage";
 import authReducer from "./features/authSlice";
+import favouritesReducer from "./features/favoritesSlice";
+
+const initialFavourites = getFavouritesFromLocalStorage() || [];
+
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
+    favourites: favouritesReducer,
+  },
+
+  preloadedState: {
+    favourites: initialFavourites,
   },
 
   middleware: (getDefaultMiddleware) =>
