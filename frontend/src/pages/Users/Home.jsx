fad94c498afbd3a6d44fcff59874c80c0009760d
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-14 16:43:52 +0530

    Updated the home component

--- frontend/src/pages/Users/Home.jsx
+++ frontend/src/pages/Users/Home.jsx
@@ -11,36 +11,32 @@ const Home = () => {
 
   return (
     <>
-      {!keyword ? <Header /> : null}
+      {!keyword && <Header />}
+
       {isLoading ? (
         <Loader />
       ) : isError ? (
         <Message variant="danger">
-          {isError?.data.message || isError.error}
+          {isError?.data?.message || isError?.error}
         </Message>
       ) : (
         <>
-          <div className="flex justify-between items-center">
-            <h1 className="ml-[20rem] mt-[10rem] text-[3rem]">
-              Special Products
-            </h1>
-
+          <div className="flex justify-center gap-9 items-center mt-12 mx-20">
+            <h1 className="text-4xl font-bold">Special Products</h1>
             <Link
               to="/shop"
-              className="bg-pink-600 font-bold rounded-full py-2 px-10 mr-[18rem] mt-[10rem]"
+              className="bg-pink-600 text-white font-bold rounded-full py-2 px-8"
             >
               Shop
             </Link>
           </div>
 
-          <div>
-            <div className="flex justify-center flex-wrap mt-[2rem]">
-              {data.products.map((product) => (
-                <div key={product._id}>
-                  <Product product={product} />
-                </div>
-              ))}
-            </div>
+          <div className="flex justify-center flex-wrap mt-8 mx-20">
+            {data?.products.map((product) => (
+              <div key={product._id} className="p-4">
+                <Product product={product} />
+              </div>
+            ))}
           </div>
         </>
       )}
