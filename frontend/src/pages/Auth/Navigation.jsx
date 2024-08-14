546987d027e380ca634cd82026137d315c633b4a
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-14 22:13:22 +0530

    Updated the navigation component

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -20,7 +20,7 @@ const Navigation = () => {
 
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [showSidebar, setShowSidebar] = useState(false);
-  const dropdownRef = useRef(null); // Ref for the dropdown
+  const dropdownRef = useRef(null);
 
   const handleMouseEnter = () => {
     setDropdownOpen(true);
@@ -45,7 +45,6 @@ const Navigation = () => {
     }
   };
 
-  // Close dropdown when navigating to another link
   const handleLinkClick = () => {
     setDropdownOpen(false);
   };
@@ -55,10 +54,10 @@ const Navigation = () => {
       style={{ zIndex: 9999 }}
       className={`${
         showSidebar ? "hidden" : "flex"
-      } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] w-[4%] hover:w-[15%] h-[100vh]  fixed `}
+      } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] w-[4%] hover:w-[15%] h-[100vh] fixed transition-all duration-500 ease-in-out`} // Added smooth transition
       id="navigation-container"
     >
-      <div className="flex flex-col justify-center space-y-4 transition-all duration-500 ease-in-out">
+      <div className="flex flex-col justify-center space-y-4">
         <button
           className="xl:hidden lg:hidden md:block"
           onClick={() => setShowSidebar(!showSidebar)}
@@ -67,21 +66,21 @@ const Navigation = () => {
         </button>
 
         <Link to="/" className="flex relative" onClick={handleLinkClick}>
-          <div className="flex justify-center items-center transition-transform transform hover:translate-x-2">
+          <div className="flex justify-center items-center transition-transform transform hover:translate-x-2 duration-300 ease-in-out">
             <AiOutlineHome className="mt-[3rem] mr-2" size={20} />
             <span className="hidden nav-item-name mt-[3rem]">HOME</span>
           </div>
         </Link>
 
         <Link to="/shop" className="flex relative" onClick={handleLinkClick}>
-          <div className="flex justify-center items-center transition-transform transform hover:translate-x-2">
+          <div className="flex justify-center items-center transition-transform transform hover:translate-x-2 duration-300 ease-in-out">
             <AiOutlineShopping className="mt-[3rem] mr-2" size={20} />
             <span className="hidden nav-item-name mt-[3rem]">SHOP</span>
           </div>
         </Link>
 
         <Link to="/cart" className="flex relative" onClick={handleLinkClick}>
-          <div className="flex justify-center items-center transition-transform transform hover:translate-x-2">
+          <div className="flex justify-center items-center transition-transform transform hover:translate-x-2 duration-300 ease-in-out">
             <AiOutlineShoppingCart className="mt-[3rem] mr-2" size={20} />
             <span className="hidden nav-item-name mt-[3rem]">CART</span>
           </div>
@@ -91,9 +90,9 @@ const Navigation = () => {
           className="flex relative"
           onClick={handleLinkClick}
         >
-          <div className="flex justify-center items-center transition-transform transform hover:translate-x-2">
+          <div className="flex justify-center items-center transition-transform transform hover:translate-x-2 duration-300 ease-in-out">
             <FaHeart className="mt-[3rem] mr-2" size={20} />
-            <span className="hidden nav-item-name mt-[3rem]">Favorites</span>
+            <span className="hidden nav-item-name mt-[3rem]">FAVOURITES</span>
           </div>
         </Link>
       </div>
@@ -104,7 +103,7 @@ const Navigation = () => {
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
-        <button className="flex items-center text-gray-800 focus:outline-none">
+        <button className="flex items-center text-gray-800 focus:outline-none transition-transform transform hover:scale-105 duration-300 ease-in-out">
           {userInfo ? (
             <span className="text-white">{userInfo.username}</span>
           ) : (
@@ -115,7 +114,7 @@ const Navigation = () => {
               xmlns="http://www.w3.org/2000/svg"
               className={`h-4 w-4 ml-1 ${
                 dropdownOpen ? "transform rotate-180" : ""
-              }`}
+              } transition-transform duration-300 ease-in-out`}
               fill="none"
               viewBox="0 0 24 24"
               stroke="white"
@@ -132,9 +131,9 @@ const Navigation = () => {
 
         {dropdownOpen && userInfo && (
           <ul
-            className={`absolute bottom-0 mt-2 mr-14 pb-4 space-y-2 bg-white text-gray-600 ${
-              !userInfo.isAdmin ? "-top-20" : "-top-80"
-            } `}
+            className={`absolute bottom-0 mt-2 mr-14 pb-4 space-y-2 bg-white text-gray-600 transition-opacity duration-300 ease-in-out opacity-0 ${
+              dropdownOpen ? "opacity-100" : "opacity-0"
+            } ${!userInfo.isAdmin ? "-top-20" : "-top-80"}`}
           >
             {userInfo.isAdmin && (
               <>
@@ -213,7 +212,7 @@ const Navigation = () => {
             <li>
               <Link
                 to="/login"
-                className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
+                className="flex items-center mt-5 transition-transform transform hover:translate-x-2 duration-300 ease-in-out"
                 onClick={handleLinkClick}
               >
                 <AiOutlineLogin className="mr-2 mt-[4px]" size={26} />
@@ -223,7 +222,7 @@ const Navigation = () => {
             <li>
               <Link
                 to="/register"
-                className="flex items-center mt-5 transition-transform transform hover:translate-x-2"
+                className="flex items-center mt-5 transition-transform transform hover:translate-x-2 duration-300 ease-in-out"
                 onClick={handleLinkClick}
               >
                 <AiOutlineUserAdd size={26} />
