d6690f306272fe5b7bb42292ff937fc1c45de22c
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-14 15:23:44 +0530

    Fixed bugs to show favourites in local storage

--- frontend/src/utils/localStorage.js
+++ frontend/src/utils/localStorage.js
@@ -1,23 +1,32 @@
-//Add a product to local storage
+// Add a product to local storage
 export const addFavouriteToLocalStorage = (product) => {
-  const favourites = getFavouritesFromLocalStorage() || [];
-  if (favourites.some((p) => p._id === product._id)) {
+  // Retrieve existing favourites from local storage
+  const favourites = getFavouritesFromLocalStorage();
+
+  // Check if the product is already in the favourites
+  if (!favourites.some((p) => p._id === product._id)) {
     favourites.push(product);
     localStorage.setItem("favourites", JSON.stringify(favourites));
   }
 };
 
-//Remove product from Local Storage
+// Remove product from local storage
 export const removeFavouriteFromLocalStorage = (productId) => {
-  const favourites = getFavouritesFromLocalStorage() || [];
-  const updateFavourites = favourites.filter((p) => p._id !== productId);
+  // Retrieve existing favourites from local storage
+  const favourites = getFavouritesFromLocalStorage();
 
-  localStorage.setItem("favourites", JSON.stringify(updateFavourites));
+  // Filter out the product to be removed
+  const updatedFavourites = favourites.filter((p) => p._id !== productId);
+
+  // Update local storage
+  localStorage.setItem("favourites", JSON.stringify(updatedFavourites));
 };
 
-//Retrieve Favourites from Local Storage
+// Retrieve favourites from local storage
 export const getFavouritesFromLocalStorage = () => {
+  // Retrieve favourites JSON string from local storage
   const favouritesJSON = localStorage.getItem("favourites");
 
+  // Parse and return favourites array, or return empty array if not found
   return favouritesJSON ? JSON.parse(favouritesJSON) : [];
 };
