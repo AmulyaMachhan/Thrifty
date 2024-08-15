4fa4e5cc9e239625d61a1a2a571d6d89239d4156
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-15 13:43:17 +0530

    Rended the favourites count component

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -14,6 +14,7 @@ import "./Navigation.css";
 import { useSelector, useDispatch } from "react-redux";
 import { useLogoutMutation } from "../../redux/api/userApiSlice.js";
 import { logout } from "../../redux/features/authSlice.js";
+import FavoritesCount from "../Products/FavouritesCount.jsx";
 
 const Navigation = () => {
   const { userInfo } = useSelector((state) => state.auth);
@@ -90,9 +91,10 @@ const Navigation = () => {
           className="flex relative"
           onClick={handleLinkClick}
         >
-          <div className="flex justify-center items-center transition-transform transform hover:translate-x-2 duration-300 ease-in-out">
+          <div className="flex justify-center items-center relative transition-transform transform hover:translate-x-2 duration-300 ease-in-out">
             <FaHeart className="mt-[3rem] mr-2" size={20} />
             <span className="hidden nav-item-name mt-[3rem]">FAVOURITES</span>
+            <FavoritesCount />
           </div>
         </Link>
       </div>
