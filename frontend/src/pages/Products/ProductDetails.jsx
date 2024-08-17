aec881d3901ba04dd645bb037c48fa6b5529d05d
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-17 14:49:51 +0530

    Updated the product details component

--- frontend/src/pages/Products/ProductDetails.jsx
+++ frontend/src/pages/Products/ProductDetails.jsx
@@ -2,14 +2,9 @@ import { useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import Loader from "../../components/Loader";
 import Message from "../../components/Message";
-import {
-  FaBox,
-  FaClock,
-  FaShoppingCart,
-  FaStar,
-  FaStore,
-} from "react-icons/fa";
-import moment from "moment";
+import ProductInfo from "./ProductInfo";
+import ProductActions from "./ProductActions";
+import ProductTabs from "./ProductTabs";
 import HeartIcon from "./HeartIcon";
 import {
   useCreateReviewMutation,
@@ -17,7 +12,6 @@ import {
 } from "../../redux/api/productApiSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
-import ProductTabs from "./ProductTabs";
 
 const ProductDetails = () => {
   const dispatch = useDispatch();
@@ -40,10 +34,8 @@ const ProductDetails = () => {
 
   const submitHandler = async (e) => {
     e.preventDefault();
-
     try {
       const res = await createReview({ productId, rating, comment }).unwrap();
-
       refetch();
       toast.success(res.message);
     } catch (error) {
@@ -52,20 +44,18 @@ const ProductDetails = () => {
   };
 
   const addToCartHandler = () => {
-    dispatch();
+    dispatch(); // Add your dispatch action here
     navigate("/cart");
   };
 
   return (
     <>
-      <div>
-        <Link
-          to="/"
-          className="text-white font-semibold hover:underline ml-[10rem]"
-        >
-          Go Back
-        </Link>
-      </div>
+      <Link
+        to="/"
+        className="text-white font-semibold hover:underline ml-[10rem]"
+      >
+        Go Back
+      </Link>
 
       {isLoading ? (
         <Loader />
@@ -74,101 +64,42 @@ const ProductDetails = () => {
           {error?.data?.message || error.message}
         </Message>
       ) : (
-        <>
-          <div className="flex flex-wrap relative items-between mt-[2rem] ml-[10rem]">
-            <div>
-              <img
-                src={product.image}
-                alt={product.name}
-                className="w-full xl:w-[50rem] lg:w-[45rem] md:w-[30rem] sm:w-[20rem] mr-[2rem]"
-              />
-
-              <HeartIcon product={product} />
-            </div>
-
-            <div className="flex flex-col justify-between">
-              <h2 className="text-2xl font-semibold">{product.name}</h2>
-              <p className="my-4 xl:w-[35rem] lg:w-[35rem] md:w-[30rem] text-[#B0B0B0]">
-                {product.description}
-              </p>
-
-              <p className="text-5xl my-4 font-extrabold">$ {product.price}</p>
-
-              <div className="flex items-center justify-between w-[20rem]">
-                <div className="one">
-                  <h1 className="flex items-center mb-6">
-                    <FaStore className="mr-2 text-white" /> Brand:{" "}
-                    {product.brand}
-                  </h1>
-                  <h1 className="flex items-center mb-6 w-[20rem]">
-                    <FaClock className="mr-2 text-white" /> Added:{" "}
-                    {moment(product.createdAt).fromNow()}
-                  </h1>
-                  <h1 className="flex items-center mb-6">
-                    <FaStar className="mr-2 text-white" /> Reviews:{" "}
-                    {product.numReviews}
-                  </h1>
-                </div>
-
-                <div className="two">
-                  <h1 className="flex items-center mb-6">
-                    <FaStar className="mr-2 text-white" /> Ratings: {rating}
-                  </h1>
-                  <h1 className="flex items-center mb-6">
-                    <FaShoppingCart className="mr-2 text-white" /> Quantity:{" "}
-                    {product.quantity}
-                  </h1>
-                  <h1 className="flex items-center mb-6 w-[10rem]">
-                    <FaBox className="mr-2 text-white" /> In Stock:{" "}
-                    {product.countInStock}
-                  </h1>
-                </div>
-              </div>
-
-              <div className="flex justify-between flex-wrap">
-                {product.countInStock > 0 && (
-                  <div>
-                    <select
-                      value={qty}
-                      onChange={(e) => setQty(e.target.value)}
-                      className="p-2 w-[6rem] rounded-lg text-black"
-                    >
-                      {[...Array(product.countInStock).keys()].map((x) => (
-                        <option key={x + 1} value={x + 1}>
-                          {x + 1}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                )}
-              </div>
-
-              <div className="btn-container">
-                <button
-                  onClick={addToCartHandler}
-                  disabled={product.countInStock === 0}
-                  className="bg-pink-600 text-white py-2 px-4 rounded-lg mt-4 md:mt-0"
-                >
-                  Add To Cart
-                </button>
-              </div>
-            </div>
-
-            <div className="mt-[5rem] container flex flex-wrap items-start justify-between ml-[10rem]">
-              <ProductTabs
-                loadingProductReview={loadingProductReview}
-                userInfo={userInfo}
-                submitHandler={submitHandler}
-                rating={rating}
-                setRating={setRating}
-                comment={comment}
-                setComment={setComment}
-                product={product}
-              />
-            </div>
+        <div className="flex flex-wrap justify-center w-full gap-4 relative mt-[2rem]">
+          <img
+            src={product.image}
+            alt={product.name}
+            className="w-full xl:w-[35rem] lg:w-[40rem] md:w-[35rem] sm:w-[15rem]"
+          />
+          <HeartIcon product={product} />
+          <div className="flex flex-col gap-2">
+            <h2 className="text-2xl font-semibold">{product.name}</h2>
+            <p className=" xl:w-[30rem] lg:w-[25rem] md:w-[20rem] text-[#B0B0B0]">
+              {product.description}
+            </p>
+            <p className="text-3xl font-extrabold">$ {product.price}</p>
+            <ProductInfo product={product} rating={rating} />
+            <ProductActions
+              product={product}
+              qty={qty}
+              setQty={setQty}
+              addToCartHandler={addToCartHandler}
+            />
           </div>
-        </>
+        </div>
       )}
+
+      <div className="mt-[5rem] container flex flex-wrap items-start justify-between ml-[10rem]">
+        <ProductTabs
+          loadingProductReview={loadingProductReview}
+          userInfo={userInfo}
+          submitHandler={submitHandler}
+          rating={rating}
+          setRating={setRating}
+          comment={comment}
+          setComment={setComment}
+          product={product}
+        />
+      </div>
     </>
   );
 };
