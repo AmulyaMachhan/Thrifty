c98e4abfcea0339109102ed652ef36ca913361f6
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-17 14:49:51 +0530

    configured the tailwind file to add more screens

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,12 @@ import flowbite from "flowbite-react/tailwind";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "2000px",
+        "2xl": { max: "1535px" },
+      },
+    },
   },
   plugins: [flowbitePlugin, flowbite.plugin()],
 };
