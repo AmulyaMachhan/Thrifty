d12046f7041aa279df1c97970b15bc3545dc8134
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-17 12:15:23 +0530

    Updated the product component

--- frontend/src/pages/Products/Product.jsx
+++ frontend/src/pages/Products/Product.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { Card, Button } from "flowbite-react";
 import PropTypes from "prop-types";
 import HeartIcon from "./HeartIcon";
-import Rating from "./Rating";
+import Ratings from "./Ratings";
 
 const Product = ({ product }) => {
   return (
@@ -25,15 +25,15 @@ const Product = ({ product }) => {
           {product.name}
         </h5>
 
-        <Rating rating={product.rating} />
+        <Ratings value={product.rating} />
 
         <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-white">
+          <span className="text-2xl font-bold text-white">
             ${product.price}
           </span>
           <Link
             to={`/product/${product._id}`}
-            className="rounded-lg px-5 py-2 text-center text-xl font-medium text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-cyan-800"
+            className="rounded-lg px-2 text-center text-xl font-medium text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-cyan-800"
           >
             <Button color="primary">View Product</Button>
           </Link>
