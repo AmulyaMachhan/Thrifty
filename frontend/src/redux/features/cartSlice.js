28cd75d1dcf2639a428309c3d84dfb3d62fd0d4a
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-19 10:55:44 +0530

    Created remove cart reducer

--- frontend/src/redux/features/cartSlice.js
+++ frontend/src/redux/features/cartSlice.js
@@ -19,5 +19,10 @@ const cartSlice = createSlice({
 
       return updateCart(state, item);
     },
+
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+      return updateCart(state);
+    },
   },
 });
