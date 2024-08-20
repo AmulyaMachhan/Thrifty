0a1d522599cf2b15dea992232fcba5bf5580046f
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-20 19:10:57 +0530

    Added the cart count component in navigation

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -15,6 +15,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useLogoutMutation } from "../../redux/api/userApiSlice.js";
 import { logout } from "../../redux/features/authSlice.js";
 import FavoritesCount from "../Products/FavouritesCount.jsx";
+import CartCount from "../Cart/CartCount.jsx";
 
 const Navigation = () => {
   const { userInfo } = useSelector((state) => state.auth);
@@ -85,7 +86,9 @@ const Navigation = () => {
             <AiOutlineShoppingCart className="mt-[3rem] mr-2" size={20} />
             <span className="hidden nav-item-name mt-[3rem]">CART</span>
           </div>
+          <CartCount />
         </Link>
+
         <Link
           to="/favourites"
           className="flex relative"
