b20044d8885faa29be80d67408c5e4db59312a12
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-20 10:19:37 +0530

    Fixed bugs

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import { Link, useParams } from "react-router-dom";
-import { useGetProductsQuery } from "../../redux/api/productApiSlice";
-import Loader from "../../components/Loader";
-import Message from "../../components/Message";
-import Header from "../../components/Header";
-import Product from "../Products/Product";
+import { useGetProductsQuery } from "../redux/api/productApiSlice";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import Header from "../components/Header";
+import Product from "./Products/Product";
 
 const Home = () => {
   const { keyword } = useParams();
