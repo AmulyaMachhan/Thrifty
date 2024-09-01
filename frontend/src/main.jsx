53fa87902db73d9a850965a999ed24baf6e9d3ea
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-01 09:08:24 +0530

    Updated main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,6 @@ import "./index.css";
 import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
 
-import { NextUIProvider } from "@nextui-org/react";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 
@@ -81,9 +80,7 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <NextUIProvider>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </NextUIProvider>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
