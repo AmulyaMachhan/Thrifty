987380a08de1044d1d0008197a34c4e57fe00c0a
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-01 11:07:13 +0530

    Updated the product controller

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -89,7 +89,7 @@ const removeProduct = asyncHandler(async (req, res) => {
 
 const fetchProducts = asyncHandler(async (req, res) => {
   try {
-    const pageSize = 8;
+    const pageSize = 16;
     const page = Number(req.query.page) || 1;
 
     const keyword = req.query.keyword
