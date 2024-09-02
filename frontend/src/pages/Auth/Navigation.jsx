5a8a049108fcdcda52a9c355fc9ebe063923785b
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-02 13:57:52 +0530

    Upgraded Navigation

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -1,11 +1,10 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import {
   AiOutlineHome,
   AiOutlineShopping,
   AiOutlineLogin,
   AiOutlineUserAdd,
   AiOutlineShoppingCart,
-  AiOutlineMenu,
 } from "react-icons/ai";
 import { FaHeart } from "react-icons/fa";
 import { Link } from "react-router-dom";
@@ -19,22 +18,9 @@ import FavoritesCount from "../Favourites/FavouritesCount.jsx";
 
 const Navigation = () => {
   const { userInfo } = useSelector((state) => state.auth);
-
   const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [showSidebar, setShowSidebar] = useState(false);
-  const dropdownRef = useRef(null);
-
-  const handleMouseEnter = () => {
-    setDropdownOpen(true);
-  };
-
-  const handleMouseLeave = () => {
-    setDropdownOpen(false);
-  };
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const [logoutApiCall] = useLogoutMutation();
 
   const logoutHandler = async () => {
@@ -47,197 +33,190 @@ const Navigation = () => {
     }
   };
 
+  const handleMouseEnter = () => {
+    setDropdownOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setDropdownOpen(false);
+  };
+
   const handleLinkClick = () => {
     setDropdownOpen(false);
   };
 
   return (
-    <div
-      style={{ zIndex: 9999 }}
-      className={`${
-        showSidebar ? "hidden" : "flex"
-      } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] w-[4%] hover:w-[15%] h-[100vh] fixed transition-all duration-500 ease-in-out`} // Added smooth transition
-      id="navigation-container"
-    >
-      <div className="flex flex-col justify-center space-y-4">
-        <button
-          className="xl:hidden lg:hidden md:block"
-          onClick={() => setShowSidebar(!showSidebar)}
-        >
-          <AiOutlineMenu className="mr-2 mt-[1rem]" size={22} />
-        </button>
+    <nav className="bg-black text-white fixed w-full top-0 left-0 z-50">
+      <div className="container mx-auto flex items-center justify-between p-4">
+        <div>
+          <span className="text-white text-4xl font-abril font-semibold">
+            thrifty!
+          </span>
+        </div>
 
-        <Link to="/" className="flex relative" onClick={handleLinkClick}>
-          <div className="flex justify-center items-center transition-transform transform hover:translate-x-2 duration-300 ease-in-out">
-            <AiOutlineHome className="mt-[3rem] mr-2" size={20} />
-            <span className="hidden nav-item-name mt-[3rem]">HOME</span>
-          </div>
-        </Link>
-
-        <Link to="/shop" className="flex relative" onClick={handleLinkClick}>
-          <div className="flex justify-center items-center transition-transform transform hover:translate-x-2 duration-300 ease-in-out">
-            <AiOutlineShopping className="mt-[3rem] mr-2" size={20} />
-            <span className="hidden nav-item-name mt-[3rem]">SHOP</span>
-          </div>
-        </Link>
-
-        <Link to="/cart" className="flex relative" onClick={handleLinkClick}>
-          <div className="flex justify-center items-center transition-transform transform hover:translate-x-2 duration-300 ease-in-out">
-            <AiOutlineShoppingCart className="mt-[3rem] mr-2" size={20} />
-            <span className="hidden nav-item-name mt-[3rem]">CART</span>
-            <CartCount />
-          </div>
-        </Link>
-
-        <Link
-          to="/favourites"
-          className="flex relative"
-          onClick={handleLinkClick}
-        >
-          <div className="flex justify-center items-center relative transition-transform transform hover:translate-x-2 duration-300 ease-in-out">
-            <FaHeart className="mt-[3rem] mr-2" size={20} />
-            <span className="hidden nav-item-name mt-[3rem]">FAVOURITES</span>
-            <FavoritesCount />
-          </div>
-        </Link>
-      </div>
-
-      <div
-        className="relative"
-        ref={dropdownRef}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        <button className="flex items-center text-gray-800 focus:outline-none transition-transform transform hover:scale-105 duration-300 ease-in-out">
-          {userInfo ? (
-            <span className="text-white">{userInfo.username}</span>
-          ) : (
-            <></>
-          )}
-          {userInfo && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className={`h-4 w-4 ml-1 ${
-                dropdownOpen ? "transform rotate-180" : ""
-              } transition-transform duration-300 ease-in-out`}
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="white"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d={dropdownOpen ? "M5 15l7-7 7 7" : "M19 9l-7 7-7-7"}
-              />
-            </svg>
-          )}
-        </button>
-
-        {dropdownOpen && userInfo && (
-          <ul
-            className={`absolute bottom-0 mt-2 mr-14 pb-4 space-y-2 bg-white text-gray-600 transition-opacity duration-300 ease-in-out opacity-0 ${
-              dropdownOpen ? "opacity-100" : "opacity-0"
-            } ${!userInfo.isAdmin ? "-top-20" : "-top-80"}`}
+        <div className="flex items-center space-x-4 text-sm">
+          <Link to="/" className="flex items-center" onClick={handleLinkClick}>
+            <AiOutlineHome className="text-white" size={16} />
+            <span className="ml-2">HOME</span>
+          </Link>
+          <Link
+            to="/shop"
+            className="flex items-center"
+            onClick={handleLinkClick}
           >
-            {userInfo.isAdmin && (
-              <>
-                <li>
-                  <Link
-                    to="/admin/dashboard"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                    onClick={handleLinkClick}
-                  >
-                    Dashboard
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/admin/productlist"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                    onClick={handleLinkClick}
-                  >
-                    Products
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/admin/categorylist"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                    onClick={handleLinkClick}
-                  >
-                    Category
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/admin/orderlist"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                    onClick={handleLinkClick}
-                  >
-                    Orders
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/admin/userlist"
-                    className="block px-4 py-2 hover:bg-gray-100"
-                    onClick={handleLinkClick}
-                  >
-                    Users
-                  </Link>
-                </li>
-              </>
-            )}
+            <AiOutlineShopping className="text-white" size={16} />
+            <span className="ml-2">SHOP</span>
+          </Link>
+          <Link
+            to="/cart"
+            className="flex items-center"
+            onClick={handleLinkClick}
+          >
+            <AiOutlineShoppingCart className="text-white" size={16} />
+            <span className="ml-2">CART</span>
+            <CartCount />
+          </Link>
+          <Link
+            to="/favourites"
+            className="flex items-center"
+            onClick={handleLinkClick}
+          >
+            <FaHeart className="text-white" size={16} />
+            <span className="ml-2">FAVOURITES</span>
+            <FavoritesCount />
+          </Link>
+        </div>
 
-            <li>
-              <Link
-                to="/profile"
-                className="block px-4 py-2 hover:bg-gray-100"
-                onClick={handleLinkClick}
+        <div className="relative">
+          <button
+            className="flex items-center text-gray-200 focus:outline-none"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          >
+            {userInfo ? (
+              <span className="mr-2">{userInfo.username}</span>
+            ) : (
+              <></>
+            )}
+            {userInfo && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className={`h-4 w-4 ${dropdownOpen ? "transform rotate-180" : ""}`}
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="white"
               >
-                Profile
-              </Link>
-            </li>
-            <li>
-              <button
-                onClick={() => {
-                  logoutHandler();
-                  handleLinkClick();
-                }}
-                className="block w-full px-4 py-2 text-left hover:bg-gray-100"
-              >
-                Logout
-              </button>
-            </li>
-          </ul>
-        )}
-        {!userInfo && (
-          <ul>
-            <li>
-              <Link
-                to="/login"
-                className="flex items-center mt-5 transition-transform transform hover:translate-x-2 duration-300 ease-in-out"
-                onClick={handleLinkClick}
-              >
-                <AiOutlineLogin size={26} />
-                <span className="hidden nav-item-name">LOGIN</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/register"
-                className="flex items-center mt-5 transition-transform transform hover:translate-x-2 duration-300 ease-in-out"
-                onClick={handleLinkClick}
-              >
-                <AiOutlineUserAdd size={26} />
-                <span className="hidden nav-item-name">REGISTER</span>
-              </Link>
-            </li>
-          </ul>
-        )}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d={dropdownOpen ? "M5 15l7-7 7 7" : "M19 9l-7 7-7-7"}
+                />
+              </svg>
+            )}
+          </button>
+
+          {dropdownOpen && userInfo && (
+            <ul className="absolute right-0 mt-2 bg-white text-gray-600 rounded shadow-lg w-48">
+              {userInfo.isAdmin && (
+                <>
+                  <li>
+                    <Link
+                      to="/admin/dashboard"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                      onClick={handleLinkClick}
+                    >
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/admin/productlist"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                      onClick={handleLinkClick}
+                    >
+                      Products
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/admin/categorylist"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                      onClick={handleLinkClick}
+                    >
+                      Category
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/admin/orderlist"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                      onClick={handleLinkClick}
+                    >
+                      Orders
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/admin/userlist"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                      onClick={handleLinkClick}
+                    >
+                      Users
+                    </Link>
+                  </li>
+                </>
+              )}
+
+              <li>
+                <Link
+                  to="/profile"
+                  className="block px-4 py-2 hover:bg-gray-100"
+                  onClick={handleLinkClick}
+                >
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={() => {
+                    logoutHandler();
+                    handleLinkClick();
+                  }}
+                  className="block w-full px-4 py-2 text-left hover:bg-gray-100"
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
+          )}
+          {!userInfo && (
+            <ul className="absolute right-0 mt-2 bg-white text-gray-600 rounded shadow-lg w-48">
+              <li>
+                <Link
+                  to="/login"
+                  className="flex items-center px-4 py-2 hover:bg-gray-100"
+                  onClick={handleLinkClick}
+                >
+                  <AiOutlineLogin size={26} />
+                  <span className="ml-2">LOGIN</span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/register"
+                  className="flex items-center px-4 py-2 hover:bg-gray-100"
+                  onClick={handleLinkClick}
+                >
+                  <AiOutlineUserAdd size={26} />
+                  <span className="ml-2">REGISTER</span>
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
