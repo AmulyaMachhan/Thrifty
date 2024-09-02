210928f8aff1b2a06c6a4ecdeb61b01bf9dcef0a
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-02 15:23:45 +0530

    Configured UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { ToastContainer } from "react-toastify";
 import { Outlet } from "react-router-dom";
 import Navigation from "./pages/Auth/Navigation.jsx";
 import "react-toastify/dist/ReactToastify.css";
+import Brand from "./components/Brand.jsx";
 
 function App() {
   return (
     <>
       <ToastContainer />
+      <Brand />
       <Navigation />
       <main className="py-3">
         <Outlet />
