be84c2e865395552047fbd80debfd0593c6dcf2c
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-07 01:45:36 +0530

    Updated User List

--- frontend/src/pages/Admin/UserList.jsx
+++ frontend/src/pages/Admin/UserList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { FaTrash, FaEdit } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
 import {
   useDeleteUserMutation,
   useGetUsersQuery,
@@ -9,13 +9,14 @@ import Message from "../../components/Message";
 import { toast } from "react-toastify";
 import AdminMenu from "./AdminMenu";
 import EditUserModal from "./Modals/EditUserModal";
+import { MdEdit } from "react-icons/md";
 
 function UserList() {
   const { data: users, isLoading, error, refetch } = useGetUsersQuery();
   const [deleteUser] = useDeleteUserMutation();
 
-  const [selectedUser, setSelectedUser] = useState(null); // To store the selected user for editing
-  const [isModalOpen, setIsModalOpen] = useState(false); // Modal open state
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     refetch();
@@ -23,7 +24,7 @@ function UserList() {
 
   const toggleEdit = (user) => {
     setSelectedUser(user);
-    setIsModalOpen(true); // Open the modal
+    setIsModalOpen(true);
   };
 
   const deleteHandler = async (ID) => {
@@ -51,16 +52,18 @@ function UserList() {
           {error?.data?.message || error.error}
         </Message>
       ) : (
-        <div className="flex flex-col md:flex-row py-6">
+        <div className="flex flex-col md:flex-row py-10">
           <AdminMenu />
           <div className="mx-auto">
             <table className="w-full table-auto bg-gray-800 rounded-lg shadow-lg overflow-hidden">
               <thead>
                 <tr className="bg-gradient-to-r from-gray-700 to-gray-900 text-left text-gray-400 uppercase tracking-wider">
-                  <th className="px-6 py-4">ID</th>
-                  <th className="px-6 py-4">NAME</th>
-                  <th className="px-6 py-4">EMAIL</th>
-                  <th className="px-6 py-4">ROLE</th>
+                  <th className="px-6 py-4 border-r border-r-gray-600">ID</th>
+                  <th className="px-6 py-4 border-r border-r-gray-600">NAME</th>
+                  <th className="px-6 py-4 border-r border-r-gray-600">
+                    EMAIL
+                  </th>
+                  <th className="px-6 py-4 border-r border-r-gray-600">ROLE</th>
                   <th className="px-6 py-4">ACTIONS</th>
                 </tr>
               </thead>
@@ -70,31 +73,35 @@ function UserList() {
                     key={user._id}
                     className="bg-gray-700 border-b border-gray-600 hover:bg-gray-600 transition-colors"
                   >
-                    <td className="px-6 py-3">{user._id}</td>
-                    <td className="px-6 py-2">{user.username}</td>
-                    <td className="px-6 py-2">
+                    <td className="px-6 py-3 border-r border-r-gray-600">
+                      {user._id}
+                    </td>
+                    <td className="px-6 py-2 border-r border-r-gray-600">
+                      {user.username}
+                    </td>
+                    <td className="px-6 py-2 border-r border-r-gray-600">
                       <a href={`mailto:${user.email}`}>{user.email}</a>
                     </td>
-                    <td className="px-6 py-3">
+                    <td className="px-6 py-3 border-r border-r-gray-600">
                       {user.isAdmin ? <span>Admin</span> : <span>User</span>}
                     </td>
-                    <td className="px-6 py-3 flex justify-center items-center">
-                      {!user.isAdmin && (
-                        <button
-                          onClick={() => deleteHandler(user._id)}
-                          className="bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-4 rounded-lg flex items-center"
-                        >
-                          <FaTrash />
-                        </button>
-                      )}
+                    <td className="px-6 py-3 flex items-center gap-2">
                       <button
                         onClick={() =>
                           toggleEdit(user._id, user.name, user.email)
                         }
-                        className="ml-4 text-blue-500 hover:text-blue-300"
+                        className="bg-blue-600 hover:bg-red-700 text-white font-bold py-2 px-3 rounded-lg flex items-center"
                       >
-                        <FaEdit />
+                        <MdEdit />
                       </button>
+                      {!user.isAdmin && (
+                        <button
+                          onClick={() => deleteHandler(user._id)}
+                          className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-3 rounded-lg flex items-center"
+                        >
+                          <FaTrash />
+                        </button>
+                      )}
                     </td>
                   </tr>
                 ))}
