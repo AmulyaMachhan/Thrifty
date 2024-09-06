4df39bbbdff308ebfae47ab80135a7a94fa674c5
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-06 13:27:02 +0530

    Updated Icons

--- frontend/src/Icons/EyeSlashFilledIcon.jsx
+++ frontend/src/Icons/EyeSlashFilledIcon.jsx
@@ -4,10 +4,10 @@ function EyeSlashFilledIcon(props) {
       aria-hidden="true"
       fill="none"
       focusable="false"
-      height="1em"
+      height="1rem"
       role="presentation"
       viewBox="0 0 24 24"
-      width="1em"
+      width="1rem"
       {...props}
     >
       <path
