4952d291cc04857471c4b741cc356cea881fbf61
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-06 12:59:23 +0530

    Created index file for Icons

---
+++ frontend/src/Icons/index.js
@@ -0,0 +1,4 @@
+export { default as MailIcon } from "./MailIcon";
+export { default as EyeFilledIcon } from "./EyeFilledIcon";
+export { default as EyeSlashFilledIcon } from "./EyeSlashFilledIcon";
+export { default as UserIcon } from "./UserIcon";
