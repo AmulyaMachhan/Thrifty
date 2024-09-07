0bb3a252fe0f97a92edd123f08815993694208d2
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-07 16:23:25 +0530

    Created Category Form

--- frontend/src/components/CategoryForm.jsx
+++ frontend/src/components/CategoryForm.jsx
@@ -7,25 +7,38 @@ const CategoryForm = ({
   handleDelete,
 }) => {
   return (
-    <div className="p-3">
-      <form onSubmit={handleSubmit} className="space-y-3">
-        <input
-          type="text"
-          className="py-3 px-4 border rounded-lg w-full"
-          placeholder="Write category name"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
+    <div className="bg-white shadow-lg rounded-lg p-6">
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <label
+          htmlFor="category"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Category Name
+        </label>
+        <div className="relative">
+          <input
+            type="text"
+            id="category"
+            className="block w-full py-3 px-4 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-pink-500 focus:border-pink-500 placeholder-gray-400 text-gray-700"
+            placeholder="Enter category name"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </div>
 
-        <div className="flex justify-between">
-          <button className="bg-pink-500 text-white py-2 px-4 rounded-lg hover:bg-pink-600 focus:outline-none focus:ring-2 foucs:ring-pink-500 focus:ring-opacity-50">
+        <div className="flex justify-between space-x-4">
+          <button
+            type="submit"
+            className="flex-grow bg-pink-500 text-white py-2 px-4 rounded-lg hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 shadow-lg transition-all duration-200"
+          >
             {buttonText}
           </button>
 
           {handleDelete && (
             <button
+              type="button"
               onClick={handleDelete}
-              className="bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 foucs:ring-red-500 focus:ring-opacity-50"
+              className="flex-grow bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 shadow-lg transition-all duration-200"
             >
               Delete
             </button>
