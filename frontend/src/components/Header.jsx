4aeac9713f251835c694c0e2a86cf8fcf42cd004
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-08 10:12:13 +0530

    Updated Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -15,9 +15,20 @@ const Header = () => {
   }
 
   return (
-    <div>
-      <div className="flex flex-wrap justify-evenly items-center">
-        <div className="xl:block lg:hidden md:hidden:sm:hidden">
+    <div className="">
+      <div className="py-4 bg-white">
+        <h1 className="text-4xl font-bold text-center text-black tracking-wide mb-1">
+          Featured Products
+        </h1>
+        <p className="text-lg text-center text-gray-700 font-semibold tracking-wide">
+          Discover our top picks for this season, handpicked just for you!
+        </p>
+      </div>
+      <div className="flex flex-wrap justify-evenly items-center py-8">
+        <div>
+          <h2 className="text-2xl text-center font-semibold text-gray-200 mb-6">
+            Top Picks for You
+          </h2>
           <div className="grid grid-cols-2 gap-4">
             {data.map((product) => (
               <div key={product._id}>
@@ -26,7 +37,12 @@ const Header = () => {
             ))}
           </div>
         </div>
-        <ProductCarousel />
+        <div>
+          <h2 className="text-2xl font-semibold text-gray-200 text-center mb-6">
+            Browse Our Product Carousel
+          </h2>
+          <ProductCarousel />
+        </div>
       </div>
     </div>
   );
