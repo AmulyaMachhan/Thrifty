d41a98da3dafa4377f835e7df0fa712f85a405ee
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-08 19:07:26 +0530

    Updated Product Info Component

--- frontend/src/pages/Products/ProductInfo.jsx
+++ frontend/src/pages/Products/ProductInfo.jsx
@@ -9,30 +9,38 @@ import {
 import moment from "moment";
 
 const ProductInfo = ({ product, rating }) => (
-  <div className="flex items-center justify-between">
-    <div className="one">
-      <h1 className="flex items-center mb-6">
-        <FaStore className="mr-2 text-white" /> Brand: {product.brand}
+  <div className="flex flex-col md:flex-row justify-between items-start md:items-center bg-gray-800 px-4 py-3 rounded-lg shadow-lg w-full">
+    {/* Column 1 */}
+    <div className="space-y-4 text-gray-300">
+      <h1 className="flex items-center  font-medium">
+        <FaStore className="mr-2 text-yellow-200" />
+        <span className="text-white">Brand: &nbsp;</span> {product.brand}
       </h1>
-      <h1 className="flex items-center mb-6 w-[20rem]">
-        <FaClock className="mr-2 text-white" /> Added:{" "}
+      <h1 className="flex items-center  font-medium">
+        <FaClock className="mr-2 text-yellow-200" />
+        <span className="text-white">Added: &nbsp;</span>{" "}
         {moment(product.createdAt).fromNow()}
       </h1>
-      <h1 className="flex items-center mb-6">
-        <FaStar className="mr-2 text-white" /> Reviews: {product.numReviews}
+      <h1 className="flex items-center  font-medium">
+        <FaStar className="mr-2 text-yellow-200" />
+        <span className="text-white">Reviews: &nbsp;</span> {product.numReviews}
       </h1>
     </div>
 
-    <div className="two">
-      <h1 className="flex items-center mb-6">
-        <FaStar className="mr-2 text-white" /> Ratings: {rating}
+    {/* Column 2 */}
+    <div className="space-y-4 text-gray-300 mt-6 md:mt-0">
+      <h1 className="flex items-center  font-medium">
+        <FaStar className="mr-2 text-yellow-200" />
+        <span className="text-white">Ratings: &nbsp;</span> {rating}
       </h1>
-      <h1 className="flex items-center mb-6">
-        <FaShoppingCart className="mr-2 text-white" /> Quantity:{" "}
-        {product.quantity}
+      <h1 className="flex items-center  font-medium">
+        <FaShoppingCart className="mr-2 text-yellow-200" />
+        <span className="text-white">Quantity: &nbsp;</span> {product.quantity}
       </h1>
-      <h1 className="flex items-center mb-6 w-[10rem]">
-        <FaBox className="mr-2 text-white" /> In Stock: {product.countInStock}
+      <h1 className="flex items-center  font-medium">
+        <FaBox className="mr-2 text-yellow-200" />
+        <span className="text-white">In Stock: &nbsp;</span>{" "}
+        {product.countInStock}
       </h1>
     </div>
   </div>
