0f6d1593bfcc4a741256ed891182630d35e24d98
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-09 12:18:13 +0530

    Set up backend

---
+++ backend/src/middlewares/multer.middleware.js
@@ -0,0 +1,29 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads/");
+  },
+
+  filename: (req, file, cb) => {
+    const extname = path.extname(file.originalname);
+    cb(null, `${file.fieldname}-${Date.now()}${extname}`);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const filetypes = /jpe?g|png|webp|heic/;
+  const mimetypes = /image\/jpe?g|image\/png|image\/webp|image\/heic/;
+
+  const extname = path.extname(file.originalname).toLowerCase();
+  const mimetype = file.mimetype;
+
+  if (filetypes.test(extname) && mimetypes.test(mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Images only"), false);
+  }
+};
+
+export const upload = multer({ storage, fileFilter });
