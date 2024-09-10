884e9e9cc7df29d1c01b26b51bebda6f7365099a
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-10 13:21:43 +0530

    Updated app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,13 @@ import { ToastContainer } from "react-toastify";
 import { Outlet } from "react-router-dom";
 import Navigation from "./pages/Auth/Navigation.jsx";
 import "react-toastify/dist/ReactToastify.css";
-import Brand from "./components/Brand.jsx";
 
 function App() {
   return (
     <>
       <ToastContainer />
-      <Brand />
       <Navigation />
-      <main>
+      <main className="mt-[4.5rem]">
         <Outlet />
       </main>
     </>
