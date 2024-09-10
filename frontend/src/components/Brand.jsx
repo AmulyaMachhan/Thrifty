766d3ccecf8095c3f2398c54b4b72c94509018d8
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-10 17:48:39 +0530

    Updated brand

--- frontend/src/components/Brand.jsx
+++ frontend/src/components/Brand.jsx
@@ -1,16 +1,10 @@
-import { Navbar } from "@nextui-org/react";
-
 const Brand = () => {
   return (
-    <header className="p-4 bg-black">
-      <Navbar variant="static" className="container mx-auto">
-        <div className="flex justify-center items-center w-full">
-          <span className="text-white text-4xl font-abril font-semibold">
-            thrifty!
-          </span>
-        </div>
-      </Navbar>
-    </header>
+    <div className="flex justify-center items-center w-full">
+      <span className="text-white text-4xl font-abril font-semibold">
+        thrifty!
+      </span>
+    </div>
   );
 };
 
