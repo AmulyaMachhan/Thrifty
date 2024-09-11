c7bc1b32f1243e9cb71da6733155e3c8dbeb9653
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-11 22:37:08 +0530

    Updated index file

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,10 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
-app.use(
-  "/uploads",
-  express.static(path.join(path.resolve(), "public/uploads"))
-);
+app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
 
 //Routes Import
 import userRouter from "./routes/user.routes.js";
