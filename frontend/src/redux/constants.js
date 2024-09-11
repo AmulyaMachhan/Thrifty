5f10c249ff31aafc7323de733e2475c099b4b0e8
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-11 20:12:42 +0530

    Updated constants

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -2,4 +2,4 @@ export const BASE_URL = "https://thrifty-pink.vercel.app/";
 export const USERS_URL = "/api/v1/users";
 export const CATEGORY_URL = "/api/v1/category";
 export const PRODUCT_URL = "/api/v1/products";
-export const UPLOAD_URL = "http://localhost:5000/api/v1/uploads";
+export const UPLOAD_URL = "/api/v1/uploads";
