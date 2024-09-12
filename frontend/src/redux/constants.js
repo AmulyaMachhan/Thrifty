8401cfade300e5af77d2ab09939cc3e9a225cd4d
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-12 11:46:12 +0530

    Updated constants

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://thrifty-pink.vercel.app/";
+export const BASE_URL = "https://thrifty-sigma.vercel.app";
 export const USERS_URL = "/api/v1/users";
 export const CATEGORY_URL = "/api/v1/category";
 export const PRODUCT_URL = "/api/v1/products";
