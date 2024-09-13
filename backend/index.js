8fe60ee2e908cd4c35658b21b7ed6e9f44d1209b
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-13 12:13:41 +0530

    Updated index

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,9 @@ import cookieParser from "cookie-parser";
 //UTILS
 import connectDB from "./src/config/db.js";
 
-dotenv.config();
-
-connectDB();
+dotenv.config({
+  path: "./.env",
+});
 
 const app = express();
 
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(cors());
 app.use(
   "/uploads",
-  express.static(path.join(path.resolve(), "public", "uploads"))
+  express.static(path.join(path.resolve(), "public/uploads"))
 );
 
 //Routes Import
@@ -34,6 +34,12 @@ app.use("/api/v1/category", categoryRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/uploads", uploadRouter);
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("SERVER RUNNING ON PORT :" + process.env.PORT);
-});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`SERVER SUCCESSFULLY RUNNING ON PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`MONGO DB CONNECTION ERROR ${error}`);
+  });
--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,11 +1,13 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET_KEY,
-});
+(async function () {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY,
+  });
+})();
 
 const uploadOnCloudinary = async (localFilePath) => {
   try {
