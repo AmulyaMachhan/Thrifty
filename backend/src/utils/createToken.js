f569db55d59b5d1053a23bae6eb7f49505cd9b9b
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-14 12:13:11 +0530

    Updated create token

--- backend/src/utils/createToken.js
+++ backend/src/utils/createToken.js
@@ -7,8 +7,8 @@ const createToken = (res, userID) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV || "development",
-    sameSite: "strict",
+    secure: true,
+    sameSite: "None",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
