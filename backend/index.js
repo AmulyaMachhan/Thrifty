1575436106926791c2d35e5a47afd7456950f725
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-17 10:11:19 +0530

    Updated backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,10 @@
 import dotenv from "dotenv";
-
-dotenv.config({
-  path: "./.env",
-});
-
-import path from "path";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
+
+dotenv.config();
 
 //UTILS
 import connectDB from "./src/config/db.js";
@@ -18,22 +15,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
-app.use(
-  "/uploads",
-  express.static(path.join(path.resolve(), "public/uploads"))
-);
+app.use(express.static(path.join(path.resolve() + "public")));
 
 //Routes Import
 import userRouter from "./src/routes/user.routes.js";
 import categoryRouter from "./src/routes/category.routes.js";
 import productRouter from "./src/routes/product.routes.js";
-import uploadRouter from "./src/routes/upload.routes.js";
 
 //Routes Declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/category", categoryRouter);
 app.use("/api/v1/products", productRouter);
-app.use("/api/v1/uploads", uploadRouter);
 
 connectDB()
   .then(() => {
