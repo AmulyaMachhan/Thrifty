065f7e533d284eed61c9dcc05cdc0b49c2bab3c6
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-17 13:08:34 +0530

    Deleted upload routes

--- backend/src/routes/upload.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { upload } from "../middlewares/multer.middleware.js";
-import { imageUpload } from "../controllers/upload.controller.js";
-
-const router = express.Router();
-
-router.route("/").post(upload.single("image"), imageUpload);
-
-export default router;
