47c9c7a893a85479451b6382a19922058b0e5998
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-18 14:10:14 +0530

    Created route

--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { authenticate } from "../middlewares/auth.middleware.js";
+import { createOrder } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
+router.route("/create-order").post(authenticate, createOrder);
 export default router;
