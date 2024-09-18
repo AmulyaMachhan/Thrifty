0dd4997dd43f98ee1d864008f825ceed2a21e34b
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-18 09:13:24 +0530

    Created order controller

---
+++ backend/src/controllers/order.controller.js
@@ -0,0 +1,23 @@
+function calcPrices(orderItems) {
+  const itemsPrice = orderItems.reduce(
+    (acc, item) => acc + item.price * item.qty,
+    0
+  );
+
+  const shippingPrice = itemsPrice > 100 ? 0 : 10;
+  const taxRate = 0.15;
+  const taxPrice = (itemsPrice * taxRate).toFixed(2);
+
+  const totalPrice = (
+    itemsPrice +
+    shippingPrice +
+    parseFloat(taxPrice)
+  ).toFixed(2);
+
+  return {
+    itemsPrice: itemsPrice.toFixed(2),
+    shippingPrice: shippingPrice.toFixed(2),
+    taxPrice,
+    totalPrice,
+  };
+}
