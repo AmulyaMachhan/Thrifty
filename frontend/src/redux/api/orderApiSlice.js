00faba82ccea490501278006a7741d5bb537fa93
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-20 09:23:57 +0530

    Created order api slice

---
+++ frontend/src/redux/api/orderApiSlice.js
@@ -0,0 +1,16 @@
+import { ORDER_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+const orderApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    createOrder: builder.mutation({
+      query: (order) => ({
+        url: `${ORDER_URL}`,
+        method: "POST",
+        body: order,
+      }),
+    }),
+  }),
+});
+
+export const { useCreateOrderMutation } = orderApiSlice;
