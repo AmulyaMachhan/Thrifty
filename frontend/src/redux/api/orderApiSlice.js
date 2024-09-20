1da957aa9c60ebb5d89774ec2aa4983ee336d462
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-20 10:19:56 +0530

    Injected getMyOrder endpoint

--- frontend/src/redux/api/orderApiSlice.js
+++ frontend/src/redux/api/orderApiSlice.js
@@ -14,7 +14,16 @@ const orderApiSlice = apiSlice.injectEndpoints({
     getAllOrders: builder.query({
       query: () => `${ORDER_URL}`,
     }),
+
+    getMyOrders: builder.query({
+      query: () => `${ORDER_URL}/mine`,
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
-export const { useCreateOrderMutation, useGetAllOrdersQuery } = orderApiSlice;
+export const {
+  useCreateOrderMutation,
+  useGetAllOrdersQuery,
+  useGetMyOrdersQuery,
+} = orderApiSlice;
