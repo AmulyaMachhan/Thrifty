f0a6b3e9a0e95d89d5aa25db8a7c2bc4ca27d74a
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-22 17:01:54 +0530

    Created razorpay route for creating razorpay order

--- backend/src/routes/razorpay.routes.js
+++ backend/src/routes/razorpay.routes.js
@@ -1,5 +1,7 @@
 import express from "express";
+import { createRazorpayOrder } from "../controllers/razorpay.controller.js";
 
 const router = express.Router();
 
+router.route("/create-order").post(createRazorpayOrder);
 export default router;
