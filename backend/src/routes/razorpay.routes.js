0c2133c7a42f3bc8b866b8e73af8fd58090d2d6e
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-22 17:42:08 +0530

    Created razorpay route for verify razorpay order

--- backend/src/routes/razorpay.routes.js
+++ backend/src/routes/razorpay.routes.js
@@ -1,7 +1,12 @@
 import express from "express";
-import { createRazorpayOrder } from "../controllers/razorpay.controller.js";
+import {
+  createRazorpayOrder,
+  verifyRazorpayOrder,
+} from "../controllers/razorpay.controller.js";
 
 const router = express.Router();
 
 router.route("/create-order").post(createRazorpayOrder);
+router.route("/verify-order").post(verifyRazorpayOrder);
+
 export default router;
