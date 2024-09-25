65ea427ccad48ae96e455824fe6fa467f5df7f4e
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-25 17:32:19 +0530

    Updated order list componenet

--- frontend/src/pages/Admin/OrderList.jsx
+++ frontend/src/pages/Admin/OrderList.jsx
@@ -14,6 +14,15 @@ const OrderList = () => {
         <Message variant="danger">
           {error?.data?.message || error.error}
         </Message>
+      ) : orders?.length === 0 ? (
+        <div className="text-center text-xl my-4 font-semibold tracking-wide">
+          <span>No Orders Yet. Go to&nbsp;</span>
+          <span>
+            <Link to="/shop" className="text-pink-500 underline">
+              Shop!
+            </Link>
+          </span>
+        </div>
       ) : (
         <div className="overflow-x-auto bg-black rounded-lg shadow-xl">
           <table className="min-w-full divide-y divide-gray-700">
@@ -21,49 +30,49 @@ const OrderList = () => {
               <tr>
                 <th
                   scope="col"
-                  className="p-4 text-left text-sm font-semibold text-gray-300"
+                  className="px-4 py-6 text-left text-sm font-semibold text-gray-300"
                 >
                   ITEMS
                 </th>
                 <th
                   scope="col"
-                  className="p-4 text-left text-sm font-semibold text-gray-300"
+                  className="px-4 py-6 text-left text-sm font-semibold text-gray-300"
                 >
                   ID
                 </th>
                 <th
                   scope="col"
-                  className="p-4 text-left text-sm font-semibold text-gray-300"
+                  className="px-4 py-6 text-left text-sm font-semibold text-gray-300"
                 >
                   USER
                 </th>
                 <th
                   scope="col"
-                  className="p-4 text-left text-sm font-semibold text-gray-300"
+                  className="px-4 py-6 text-left text-sm font-semibold text-gray-300"
                 >
                   DATE
                 </th>
                 <th
                   scope="col"
-                  className="p-4 text-left text-sm font-semibold text-gray-300"
+                  className="px-4 py-6 text-left text-sm font-semibold text-gray-300"
                 >
                   TOTAL
                 </th>
                 <th
                   scope="col"
-                  className="p-4 text-left text-sm font-semibold text-gray-300"
+                  className="px-4 py-6 text-left text-sm font-semibold text-gray-300"
                 >
                   PAID
                 </th>
                 <th
                   scope="col"
-                  className="p-4 text-left text-sm font-semibold text-gray-300"
+                  className="px-4 py-6 text-left text-sm font-semibold text-gray-300"
                 >
                   DELIVERED
                 </th>
                 <th
                   scope="col"
-                  className="p-4 text-left text-sm font-semibold text-gray-300"
+                  className="px-4 py-6 text-left text-sm font-semibold text-gray-300"
                 ></th>
               </tr>
             </thead>
@@ -71,28 +80,28 @@ const OrderList = () => {
               {orders.map((order) => (
                 <tr
                   key={order._id}
-                  className="hover:bg-gray-700 transition-colors"
+                  className="hover:bg-gray-900 transition-colors"
                 >
-                  <td className="p-4 whitespace-nowrap">
+                  <td className="px-4 py-6 whitespace-nowrap">
                     <img
                       src={order.orderItems[0].image}
                       alt={order._id}
                       className="w-16 h-16 object-cover rounded-md"
                     />
                   </td>
-                  <td className="p-4 whitespace-nowrap text-sm text-gray-300">
+                  <td className="px-4 py-6 whitespace-nowrap text-sm text-gray-300">
                     {order._id}
                   </td>
-                  <td className="p-4 whitespace-nowrap text-sm text-gray-300">
+                  <td className="px-4 py-6 whitespace-nowrap text-sm text-gray-300">
                     {order.user ? order.user.username : "N/A"}
                   </td>
-                  <td className="p-4 whitespace-nowrap text-sm text-gray-300">
+                  <td className="px-4 py-6 whitespace-nowrap text-sm text-gray-300">
                     {order.createdAt ? order.createdAt.substring(0, 10) : "N/A"}
                   </td>
-                  <td className="p-4 whitespace-nowrap text-sm text-gray-300">
+                  <td className="px-4 py-6 whitespace-nowrap text-sm text-gray-300">
                     $ {order.totalPrice}
                   </td>
-                  <td className="p-4 whitespace-nowrap">
+                  <td className="px-4 py-6 whitespace-nowrap">
                     {order.isPaid ? (
                       <span className="px-3 py-1 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-900 text-green-200">
                         Completed
@@ -103,7 +112,7 @@ const OrderList = () => {
                       </span>
                     )}
                   </td>
-                  <td className="p-4 whitespace-nowrap">
+                  <td className="px-4 py-6 whitespace-nowrap">
                     {order.isDelivered ? (
                       <span className="px-3 py-1 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-900 text-green-200">
                         Completed
@@ -114,7 +123,7 @@ const OrderList = () => {
                       </span>
                     )}
                   </td>
-                  <td className="p-4 whitespace-nowrap text-right text-sm font-medium">
+                  <td className="px-4 py-6 whitespace-nowrap text-right text-sm font-medium">
                     <Link to={`/order/${order._id}`}>
                       <button className="text-indigo-400 hover:text-indigo-300 transition-colors">
                         Details
