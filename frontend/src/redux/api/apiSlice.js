24e74ba40811759682dc9f3fd424ad5310eb5d8a
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-26 11:30:19 +0530

    Updated vercel.json

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants";
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL, credentials: true });
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL, credentials: "include" });
 
 export const apiSlice = createApi({
   baseQuery,
