ad5223491400de3070d97761a5e543ce32cf626c
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-27 10:13:47 +0530

    Updated backend

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://thrifty-qqkh.vercel.app/",
+    origin: "https://thrifty-qqkh.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.options(
+  "*",
+  cors({
+    origin: "https://thrifty-qqkh.vercel.app",
     credentials: true,
   })
 );
