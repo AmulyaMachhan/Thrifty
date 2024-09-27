61fc76dd426518006e7e23a83e8f6fe41222fefe
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-27 17:25:49 +0530

    Updated constants

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://thrifty-eta.vercel.app/";
+export const BASE_URL = "https://thrifty-eta.vercel.app/";
 export const USERS_URL = "/api/v1/users";
 export const CATEGORY_URL = "/api/v1/category";
 export const PRODUCT_URL = "/api/v1/products";
