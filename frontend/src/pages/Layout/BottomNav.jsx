2a6cd69fb054e108b847207dc55a1b1f72f552ca
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-28 09:17:45 +0530

    Created bottom nav

---
+++ frontend/src/pages/Layout/BottomNav.jsx
@@ -0,0 +1,11 @@
+import NavLinks from "./NavLinks";
+
+function BottomNav() {
+  return (
+    <div className="sm:hidden fixed bottom-0 left-0 right-0 bg-black flex justify-around items-center py-4 shadow-lg border-t border-gray-700">
+      <NavLinks />
+    </div>
+  );
+}
+
+export default BottomNav;
