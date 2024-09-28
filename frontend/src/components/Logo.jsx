46c7d614ae29cb9fdac12cd1e4f7363e221fa21c
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-28 08:04:59 +0530

    Update logo

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Logo = () => (
   <div>
-    <span className="text-white text-4xl font-abril font-semibold">
-      thrifty!
-    </span>
+    <Link to="/">
+      <span className="text-white text-2xl md:text-4xl font-abril font-semibold">
+        thrifty!
+      </span>
+    </Link>
   </div>
 );
 
