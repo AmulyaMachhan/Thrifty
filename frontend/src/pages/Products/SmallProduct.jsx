ef1abe007cac78f383840ede3101cf9d94f659fb
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-28 17:24:46 +0530

    Updated small products component

--- frontend/src/pages/Products/SmallProduct.jsx
+++ frontend/src/pages/Products/SmallProduct.jsx
@@ -7,8 +7,8 @@ const SmallProduct = ({ product }) => {
   const [isLoaded, setIsLoaded] = useState(false);
 
   return (
-    <div className="w-[18rem] rounded-lg">
-      <div className="relative overflow-hidden h-[12rem] w-full rounded-t-lg">
+    <div className="w-[9rem] sm:w-[18rem] rounded-lg">
+      <div className="relative overflow-hidden h-[5rem] sm:h-[12rem] w-full rounded-t-lg">
         <img
           src={product.image}
           alt={product.name}
@@ -25,8 +25,8 @@ const SmallProduct = ({ product }) => {
       <div className="py-3">
         <Link to={`/product/${product._id}`}>
           <h2 className="text-base font-semibold text-white flex justify-between items-center">
-            <span className="truncate">{product.name}</span>
-            <span className="bg-pink-600 text-white text-sm font-medium px-2 py-0.5 rounded-full">
+            <span className="truncate text-xs md:text-md">{product.name}</span>
+            <span className="bg-pink-600 text-white text-xs md:text-sm font-medium px-2 py-0.5 rounded-full">
               ${product.price}
             </span>
           </h2>
