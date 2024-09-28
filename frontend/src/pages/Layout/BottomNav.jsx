c79c696457129047fdaf88a33a6810e58306d91c
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-28 10:57:10 +0530

    Updated bottom nav

--- frontend/src/pages/Layout/BottomNav.jsx
+++ frontend/src/pages/Layout/BottomNav.jsx
@@ -2,7 +2,7 @@ import NavLinks from "./NavLinks";
 
 function BottomNav() {
   return (
-    <div className="sm:hidden fixed bottom-0 left-0 right-0 z-1000 bg-black flex justify-around items-center py-4 shadow-lg border-t border-gray-700">
+    <div className="sm:hidden fixed bottom-0 left-0 right-0 z-[1000] bg-black flex justify-around items-center py-4 shadow-lg border-t border-gray-700">
       <NavLinks />
     </div>
   );
