c36c1887a617d4616ceb97824429a9e3df69135e
AmulyaMachhan <machhanamulya@gmail.com>
2024-09-28 07:30:58 +0530

    Update app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import Navigation from "./pages/Layout/Navigation.jsx";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./pages/Layout/Footer.jsx";
+import BottomNav from "./pages/Layout/BottomNav.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main className="pt-[4.5rem] min-h-screen">
         <Outlet />
       </main>
+      <BottomNav />
       <Footer />
     </>
   );
